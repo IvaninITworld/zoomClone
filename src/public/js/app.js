9c07afe56c5ce453b48c8488d190d0bf9b7e0874
IvanInITworld <110373165+IvaninITworld@users.noreply.github.com>
2022-11-24 19:32:37 +0900

    nickname function added

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,13 +1,24 @@
 const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("form");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+}
 
 function handleOpen() {
   console.log("Connected to Server!");
 }
 socket.addEventListener("open", handleOpen);
 
+// html 쪽에 ul 이라는 리스트공간을 지정해두고,
 socket.addEventListener("message", (message) => {
+  // 소켓을 통해 메세지가 데이터가 생성될 때마다 리스트에 엘레멘트를 만들어서 올려준다
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
   console.log("New message: ", message.data);
 });
 
@@ -24,8 +35,14 @@ function handleSubmit(event) {
   // 초기회 되는 걸 막아주는 작업
   event.preventDefault();
   const input = messageForm.querySelector("input");
-  socket.send(input.value);
+  socket.send(makeMessage("new_message", input.value));
   input.value = "";
+}
+function handleNickSubmit(event) {
+  event.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
 }
 
 messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
